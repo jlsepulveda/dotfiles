4e36839951b52e6e4847143765e6e097190ba292
Jose Luis ST <josexbr@gmail.com>
2017-06-15 02:14:22 -0500

    Java JDK and JRE installations

--- ubuntu_initial_setup.sh
+++ ubuntu_initial_setup.sh
@@ -2,4 +2,5 @@ sudo apt-get install vim
 sudo apt-get install nano
 sudo apt-get install openssh-server
 sudo apt-get install openvpn
-sudo apt-get install vpnc 
+sudo apt-get install vpnc
+sudo apt-get install default-jre default-jdk 
