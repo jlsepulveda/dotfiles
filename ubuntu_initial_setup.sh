da785102302bff55416820c7baadc8856932889f
Jose Luis ST <josexbr@gmail.com>
2017-06-15 03:34:37 -0500

    Adding Subversion

--- ubuntu_initial_setup.sh
+++ ubuntu_initial_setup.sh
@@ -4,3 +4,12 @@ sudo apt-get install openssh-server
 sudo apt-get install openvpn
 sudo apt-get install vpnc
 sudo apt-get install default-jre default-jdk 
+#START SETTING UP NORD VPN
+sudo cd /etc/openvpn
+sudo wget https://nordvpn.com/api/files/zip
+sudo apt-get install ca-certificates
+sudo apt-get install unzip
+sudo unzip zip
+sudo rm zip
+#END SETTING UP NORD VPN
+sudo apt-get install subversion
