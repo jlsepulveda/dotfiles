705e41b5a167de0f2b71c700a97abb3163ac7fb4
Ubuntu <ubuntu@ip-172-31-127-171.ec2.internal>
2018-07-26 21:31:50 +0000

    R

--- ubuntu_initial_setup.sh
+++ ubuntu_initial_setup.sh
@@ -15,4 +15,5 @@ sudo rm zip
 sudo apt-get install subversion
 #encriptation
 sudo apt-get install cryptsetup
+sudo apt install r-base-core
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
