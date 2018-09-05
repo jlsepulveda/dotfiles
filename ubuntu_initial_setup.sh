fb6bbe686ca2b5d3c15813ac846693faf5f0d1fc
Jose Luis Sepulveda Tarazona <josexbr@gmail.com>
2018-09-05 12:08:41 -0500

    Adding lastpass as Password Magement

--- ubuntu_initial_setup.sh
+++ ubuntu_initial_setup.sh
@@ -42,3 +42,22 @@ sudo pip install aws-shell
 
 #Programming 
 sudo apt-get install ruby
+#Password Management 
+apt-get --no-install-recommends -yqq install \
+  bash-completion \
+  build-essential \
+  cmake \
+  libcurl4  \
+  libcurl4-openssl-dev  \
+  libssl-dev  \
+  libxml2 \
+  libxml2-dev  \
+  libssl1.1 \
+  pkg-config \
+  ca-certificates \
+  xclip
+git clone https://github.com/lastpass/lastpass-cli.git
+cd lastpass-cli
+make 
+sudo make install
+#End password Management
