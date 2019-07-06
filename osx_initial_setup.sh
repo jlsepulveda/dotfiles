80eba52f9adc343e91daa0c12f11dd7f43a5833a
Jose Luis Sepulveda Tarazona <jose.sepulveda@CO-IT005827.local>
2019-07-06 07:26:24 -0500

    autoconfirm for some ubuntu tools at installing

--- osx_initial_setup.sh
+++ osx_initial_setup.sh
@@ -4,3 +4,5 @@ brew install peco
 brew tap sachaos/todoist
 brew install todoist
 brew install tmux
+brew install awscli
+brew install htop
--- ubuntu_initial_setup.sh
+++ ubuntu_initial_setup.sh
@@ -1,43 +1,43 @@
-sudo apt-get install vim
-sudo apt-get install nano
-sudo apt-get install openssh-server
-sudo apt-get install openvpn
-sudo apt-get install vpnc
-sudo apt-get install default-jre default-jdk 
+sudo apt-get -y install vim
+sudo apt-get -y  install nano
+sudo apt-get -y  install openssh-server
+sudo apt-get -y  install openvpn
+sudo apt-get -y  install vpnc
+sudo apt-get -y  install default-jre default-jdk 
 #START SETTING UP NORD VPN
 sudo cd /etc/openvpn
 #Downloading Files
 #sudo wget https://www.privateinternetaccess.com/openvpn/openvpn.zip
-sudo apt-get install ca-certificates
-sudo apt-get install unzip
+sudo apt-get -y  install ca-certificates
+sudo apt-get -y  install unzip
 sudo unzip zip
 unzip openvpn.zip -d openvpn
 sudo rm zip
 #sudo cp openvpn/ca.rsa.2048.crt openvpn/crl.rsa.2048.pem /etc/openvpn/
 #sudo cp openvpn/Japan.ovpn /etc/openvpn/Japan.conf
 #END SETTING UP NORD VPN
-sudo apt-get install subversion
-sudo apt-get install iptables-persistent
+sudo apt-get -y install subversion
+sudo apt-get -y install iptables-persistent
 #General Tools
-sudo apt-get install zip
+sudo apt-get -y install zip
 #Installing Java
 sudo apt-get install default-jre
 #encriptation
-sudo apt-get install cryptsetup
+sudo apt-get -y install cryptsetup
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
 sudo apt-get update
-sudo apt install r-base-core
+sudo apt install -y  r-base-core
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sudo apt-get install htop
+sudo apt-get -y install htop
 #installing SDK Man to management multiples version of diferents SDKs
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 #Cloud Services
 #Amazon
-sudo apt-get install awscli
-sudo apt-get install python-pip
+sudo apt-get -y install awscli
+sudo apt-get -y  install python-pip
 sudo pip install aws-shell
 
 #Programming 
